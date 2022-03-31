df53c01b4f81365afe35e3ee82afc9e0daa41798
Bijay Regmi <54135486+bijayregmi23@users.noreply.github.com>
2022-03-31 19:12:13 +0530

    Add files via upload

---
+++ Advanced_System_Software/Lab5(Recurssive_Descent_Parser)/g2.py
@@ -0,0 +1,59 @@
+ptr = 0
+ip = input("Enter Input String : ") + '$'
+input_str = ip
+
+
+def U():
+    global ip
+    global ptr
+    if ip[ptr] == '(':
+        ptr += 1
+        S()
+        if ip[ptr] == ')':
+            ptr += 1
+            return
+        # CHECKING IF IT IS NOT ENDING WITH ) AND ENDING WITH $
+        else:
+            if ip[ptr] == '$':
+                ip = ip[:ptr]+'@'+'$'
+
+    
+    elif ip[ptr] == 'a':
+        ptr += 1
+        S()
+        if ip[ptr] == 'b':
+            ptr += 1
+    elif ip[ptr] == 'd':
+        ptr += 1
+        
+
+def V():
+    global ip
+    global ptr
+    if ip[ptr] == 'a':
+        ptr += 1
+        V()
+    else:
+        return
+
+def W():
+    global ip
+    global ptr
+    if ip[ptr] == 'c':
+        ptr += 1
+        W()
+    else:
+        return
+
+def S():
+    U()
+    V()
+    W()
+    return
+
+if __name__ == '__main__':
+    S()
+    if ip[ptr] == '$':
+        print("\n",input_str," is VALID String ", sep = '')
+    else:
+        print("\n",input_str," is NOT VALID String ", sep = '')
